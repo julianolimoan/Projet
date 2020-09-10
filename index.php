1628bda01af2afdc5e63042087a19f5e30ad473a
julianolimoan <56119872+julianolimoan@users.noreply.github.com>
2020-09-10 23:36:51 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Racourcisseur d'url</title>
+        <link rel="stylesheet" type="text/css" href="design/default.css">
+        <link rel="icon" type="image/png" href="pictures/favico.png">
+    </head>
+    <body>
+
+    <section id="hello">
+
+        <div class="container">
+
+            <header>
+                <img src="pictures/logo.png" alt="logo" id="logo">
+
+            </header>
+            <h1>Une Url longue? Raccourcissez la</h1>
+            <h2>Largement meilleur et plus court que les autres.</h2>
+            
+            <form method="post" action="../">
+                <input type="url" name="url"
+                placeholder="Collez un lien à raccourcir">
+                <input type="submit" value="raccourcir">
+        </div>
+    </section>
+    <section id="Brand">
+        <div class="container">
+            <h3> Ces marques nous font confiance</h3>
+            <img src="pictures/1.png" alt="1" class="picture">
+            <img src="pictures/2.png" alt="2" class="picture">
+            <img src="pictures/3.png" alt="3" class="picture">
+            <img src="pictures/4.png" alt="4" class="picture">
+    </section>
+
+
+    <footer>
+        <img src="pictures/logo2.png" alt="logo" id="logo">
+        <br>
+        2018 Bitly<br>
+        <a href="#">Contact</a> - <a href="#"> A propos</a>
+
+
+    </body>
+</html> 
